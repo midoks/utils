c79eeda2c1f8bec715906ce8e5acd280d627fcfe
midoks <midoks@163.com>
2022-04-11 00:42:40 +0800

    up

--- string.go
+++ string.go
@@ -1,1 +1,14 @@
 package utils
+
+import (
+// "strings"
+)
+
+func InListIsExist(source string, check []string) bool {
+	for _, s := range check {
+		if source == s {
+			return true
+		}
+	}
+	return false
+}
--- string_test.go
+++ string_test.go
@@ -5,9 +5,9 @@ import (
 	"testing"
 )
 
-// go test -bench=BenchmarkSymbolEqual -benchtime=30s -cpuprofile mapCpu.out
-// GOMAXPROCS=1 go test -bench=BenchmarkSymbolEqual -benchmem -benchtime=10s
-func BenchmarkSymbolEqual(b *testing.B) {
+// go test -bench=BenchmarkEqualSymbol -benchtime=30s -cpuprofile mapCpu.out
+// GOMAXPROCS=1 go test -bench=BenchmarkEqualSymbol -benchmem -benchtime=10s
+func BenchmarkEqualSymbol(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		t := "t"
@@ -19,13 +19,50 @@ func BenchmarkSymbolEqual(b *testing.B) {
 	b.StopTimer()
 }
 
-// go test -bench=BenchmarkStringsEqual -benchtime=30s -cpuprofile mapCpu.out
-// GOMAXPROCS=1 go test -bench=BenchmarkStringsEqual -benchmem -benchtime=10s
-func BenchmarkStringsEqual(b *testing.B) {
+// go test -bench=BenchmarkEqualStrings -benchtime=30s -cpuprofile mapCpu.out
+// GOMAXPROCS=1 go test -bench=BenchmarkEqualStrings -benchmem -benchtime=10s
+func BenchmarkEqualStrings(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		t := "t"
 		if strings.EqualFold(t, "t") {
+		} else {
+			b.Log("TestStringEqual ok")
+		}
+	}
+	b.StopTimer()
+}
+
+func InListIsExistTmp(source string, check []string) bool {
+	for _, s := range check {
+		if strings.EqualFold(source, s) {
+			return true
+		}
+	}
+	return false
+}
+
+// GOMAXPROCS=1 go test -bench=BenchmarkInListIsExistTmpEqual -benchmem -benchtime=10s
+func BenchmarkInListIsExistTmpEqual(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		t := []string{"t", "t1"}
+
+		if InListIsExistTmp("t", t) {
+		} else {
+			b.Log("TestStringEqual ok")
+		}
+	}
+	b.StopTimer()
+}
+
+// GOMAXPROCS=1 go test -bench=BenchmarkInListIsExist -benchmem -benchtime=10s
+func BenchmarkInListIsExist(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		t := []string{"t", "t1"}
+
+		if InListIsExist("t", t) {
 		} else {
 			b.Log("TestStringEqual ok")
 		}
