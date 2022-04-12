cfc1dc961f8cf292071d0c04028b192f74a6525c
midoks <midoks@163.com>
2022-04-12 19:30:37 +0800

    up

--- string.go
+++ string.go
@@ -1,9 +1,11 @@
 package utils
 
 import (
-// "strings"
+	// "strings"
+	"unsafe"
 )
 
+//Check whether the characters exist in the array
 func InListIsExist(source string, check []string) bool {
 	for _, s := range check {
 		if source == s {
@@ -11,4 +13,16 @@ func InListIsExist(source string, check []string) bool {
 		}
 	}
 	return false
+}
+
+//String to byte, only read-only
+func StringToBytes(str string) []byte {
+	x := (*[2]uintptr)(unsafe.Pointer(&str))
+	b := [3]uintptr{x[0], x[1], x[1]}
+	return *(*[]byte)(unsafe.Pointer(&b))
+}
+
+// Byte to string, only read-only
+func BytesToString(b []byte) string {
+	return *(*string)(unsafe.Pointer(&b))
 }
--- string_test.go
+++ string_test.go
@@ -41,3 +41,43 @@ func BenchmarkInListIsExist(b *testing.B) {
 	}
 	b.StopTimer()
 }
+
+// GOMAXPROCS=1 go test -bench=BenchmarkBytesToString -benchmem -benchtime=10s
+func BenchmarkBytesToString(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		t := []byte{'t', '1'}
+		_ = BytesToString(t)
+	}
+	b.StopTimer()
+}
+
+// GOMAXPROCS=1 go test -bench=BenchmarkBytesToStringLocal -benchmem -benchtime=10s
+func BenchmarkBytesToStringLocal(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		t := []byte{'t', '1'}
+		_ = string(t)
+	}
+	b.StopTimer()
+}
+
+// GOMAXPROCS=1 go test -bench=BenchmarkStringToBytes -benchmem -benchtime=3s
+func BenchmarkStringToBytes(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		t := "t1"
+		_ = StringToBytes(t)
+	}
+	b.StopTimer()
+}
+
+// GOMAXPROCS=1 go test -bench=BenchmarkStringToBytes -benchmem -benchtime=3s
+func BenchmarkStringToBytesLocal(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		t := "t1"
+		_ = []byte(t)
+	}
+	b.StopTimer()
+}
